8ad0592ac2b897ccd8d1c214475121aab501c6c2
natha <nathanm1034@tamu.edu>
2024-02-21 12:10:56 -0600

    created search bar through js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,28 @@ function setup() {
   button = createButton('click me');
   button.position(19, 19);
   button.mousePressed(loadCamera);
+  search();
+}
+
+function search() {
+  var searchInput = document.createElement('input');
+  searchInput.setAttribute('type', 'text');
+  searchInput.setAttribute('placeholder', 'Search...');
+  searchInput.id = 'searchInput'; 
+
+  var searchButton = document.createElement('button');
+  searchButton.textContent = 'Search'; 
+  searchButton.id = 'searchButton'; 
+
+  var container = document.getElementById('search-container') || document.body;
+
+  container.appendChild(searchInput);
+  container.appendChild(searchButton);
+
+  searchButton.addEventListener('click', function() {
+    var searchTerm = searchInput.value;
+    console.log('Searching for:', searchTerm);
+  });
 }
 
 function loadCamera(){
